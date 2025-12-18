befa05b32379ca6c40a4529247094feab34119b7
Bachtiar21 <bachtiar21@Bachtiar21s-MacBook-Air-1839.local>
2025-12-18 18:22:44 +0700

    Change next.config.ts v3

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/dolphin-sound",
+  assetPrefix: "/dolphin-sound",
   images: {
     unoptimized: true,
   },
